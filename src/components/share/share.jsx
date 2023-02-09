634dfc86e76372b56587f879a5cc2991272ddc42
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-02-09 14:00:02 +0100

    feat: created and styled the share component which is imported to the feed

--- src/components/share/share.jsx
+++ src/components/share/share.jsx
@@ -1,3 +1,4 @@
+import { EmojiEmotions, Label, PermMedia, Room } from "@mui/icons-material";
 import "./share.css";
 
 const Share = () => {
@@ -10,8 +11,36 @@ const Share = () => {
             alt="avi"
             className="shareProfilePicture"
           />
+          <input
+            placeholder="What are you thinking ?"
+            className="shareTopInput"
+          />
         </div>
-        <div className="shareBottom"></div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia htmlColor="tomato" className="shareOptionIcon" />
+              <span className="shareOptionText">Photo or Video</span>
+            </div>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareOptionIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="green" className="shareOptionIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions
+                htmlColor="goldenrod"
+                className="shareOptionIcon"
+              />
+              <span className="shareOptionText">Feeling</span>
+            </div>
+          </div>
+          <button className="shareButton">Share</button>
+        </div>
       </div>
     </div>
   );
