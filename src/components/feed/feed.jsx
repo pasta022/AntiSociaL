4cc0fe30723dc80dfabe2429513f7d4a6849615b
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-02-09 15:39:07 +0100

    feat: added and stlyed a post component

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,11 +1,16 @@
 import './feed.css'
 import Share from '../share/share'
+import Post from '../post/post'
 
 const Feed = () => {
   return (
     <div className='feedContainer'>
       <div className="feedWrapper">
         <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   )
---
+++ src/components/post/post.jsx
@@ -0,0 +1,46 @@
+import { MoreVert } from "@mui/icons-material";
+import "./post.css";
+
+const Post = () => {
+  return (
+    <div className="postContainer">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/10.png"
+              alt="avi"
+              className="postProfilePicture"
+            />
+            <span className="postUserName">Anonymoose</span>
+            <span className="postTimeStamp">5M</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat
+          </span>
+          <img src="/assets/posts/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="/assets/like.jpeg" alt="" className="postLikeIcon" />
+            <img src="/assets/love.jpg" alt="" className="postLikeIcon" />
+            <span className="postLikeCounter">60 people liked it</span>
+          </div> 
+          <div className="postBottomRight">
+            <div className="postCommentText">12 Comments</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
